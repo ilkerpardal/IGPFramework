136aec55f8008d30b5c7f4cce6343f5374f10a70
ilker pardal <ilker.pardal@gmail.com>
2019-07-31 21:57:09 +0300

    api yapılandırma

---
+++ IgpFramework_Api/IgpFramework.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using IgpFramework.Data.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IgpFramework.Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> ListUserAsync();
+        Task AddUserAsync(User user);
+        void UpdateUserAsync(User user);
+        Task RemoveUserAsync(int userId);
+        Task<User> FindUserAsync(int userId);
+    }
+}
